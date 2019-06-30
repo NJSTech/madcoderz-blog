d29a55a86cb2ea8b9ab0b3e4848320d5e0fd5df8
Shoeb <nawjeshbd@gmail.com>
2019-06-30 22:24:13 +0600

    UsersTableSeeder added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,10 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,8 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email_verified_at' => Carbon::now(),
+        'password' => bcrypt('abc123'), // password
         'remember_token' => Str::random(10),
     ];
 });
