7e72b549ed51a18b71c17805fa0a775ba52d0324
Shoeb <nawjeshbd@gmail.com>
2019-07-04 18:07:06 +0600

    initial css and js file configuared

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css/app.css')
+      .styles('resources/sass/custom.css', 'public/css/custom.css')
+      .js('resources/js/app.js','public/js/app.js')
+      .version();
