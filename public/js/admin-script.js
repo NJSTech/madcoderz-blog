aefa56cceb4e91da554d9d7b9cc63db3db7ca721
Shoeb <nawjeshbd@gmail.com>
2019-07-22 00:46:09 +0600

    added some css and js

--- public/js/admin-script.js
+++ public/js/admin-script.js
@@ -169,6 +169,17 @@ $(function () {
 
 /***/ }),
 
+/***/ "./resources/js/category.js":
+/*!**********************************!*\
+  !*** ./resources/js/category.js ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'F:\\madcoderz\\htdocs\\madcoderz-blog\\resources\\js\\category.js'");
+
+/***/ }),
+
 /***/ "./resources/js/dashboard.js":
 /*!***********************************!*\
   !*** ./resources/js/dashboard.js ***!
@@ -275,14 +286,15 @@ $(function () {
 /***/ }),
 
 /***/ 3:
-/*!************************************************************************!*\
-  !*** multi ./resources/js/admin-script.js ./resources/js/dashboard.js ***!
-  \************************************************************************/
+/*!***************************************************************************************************!*\
+  !*** multi ./resources/js/admin-script.js ./resources/js/dashboard.js ./resources/js/category.js ***!
+  \***************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! F:\madcoderz\htdocs\madcoderz-blog\resources\js\admin-script.js */"./resources/js/admin-script.js");
-module.exports = __webpack_require__(/*! F:\madcoderz\htdocs\madcoderz-blog\resources\js\dashboard.js */"./resources/js/dashboard.js");
+__webpack_require__(/*! F:\madcoderz\htdocs\madcoderz-blog\resources\js\dashboard.js */"./resources/js/dashboard.js");
+module.exports = __webpack_require__(/*! F:\madcoderz\htdocs\madcoderz-blog\resources\js\category.js */"./resources/js/category.js");
 
 
 /***/ })
